9924c8ee92f857be1f0c48a9d2403e0b791ef12a
ConnerCox <49455602+ConnerCox@users.noreply.github.com>
2019-06-05 14:43:07 -0600

    Add files via upload

--- Ch. 24/TestQueue.java
+++ Ch. 24/TestQueue.java
@@ -8,7 +8,7 @@ public class TestQueue {
 		GenericQueue<String> queue = new GenericQueue<>();
 		
 		//initialization
-		System.out.println("Adding names to queue");
+		System.out.println("Adding names to queue: \nbob\ntexas\ngreg\nbilly");
 		queue.enqueue("bob"); // Add it to the queue
 		queue.enqueue("texas"); 
 		queue.enqueue("greg");
@@ -16,9 +16,10 @@ public class TestQueue {
 		
 
 		// Remove elements from the queue
+		System.out.println("Removing bob and texas:");
 		System.out.println("1. " + queue.dequeue());
 		System.out.println("2. " + queue.dequeue());
-		System.out.println("3. " + queue.dequeue());
-		System.out.println("4. " + queue.dequeue());
+		
+		System.out.println("Printing whats left in the queue: " + queue);
 	}
 }
