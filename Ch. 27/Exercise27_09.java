b954afaf398aab53875450a346f5e7f1f78c7aa7
ConnerCox <49455602+ConnerCox@users.noreply.github.com>
2019-06-10 14:09:47 -0600

    Add files via upload

---
+++ Ch. 27/Exercise27_09.java
@@ -0,0 +1,27 @@
+/* 
+Author: Conner Cox
+Date: June 10th, 2019
+
+Given: "4.5"      Expected: 51451
+Given: "Hello"    Expected: 69609650
+ */
+public class Exercise27_09 { 
+	public static void main(String[] args) {
+		String s = "4.5";
+		System.out.println(hashCodeForString(s));
+
+		s = "Hello";
+		System.out.println(hashCodeForString(s));
+	}
+
+	public static int hashCodeForString(String string) {
+		//create vars
+		char[] s = string.toCharArray();
+		int hash = 0;
+		//run through char array
+		for(int i=0; i<s.length; i++) {
+			hash += (s[i] * Math.pow((31), (s.length-(i+1))));
+		}
+		return hash;
+	}
+}
---
+++ Ch. 27/Exercise27_11.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+
+public class Exercise27_11 {
+	public static void main(String[] args) {
+		Set<String> set = new HashSet<String>(); 
+		set.add("Smith");
+		set.add("Anderson");
+		set.add("Lewis");
+		set.add("Cook"); 
+		set.add("Smith");
+
+		ArrayList<String> list = setToList(set);
+		System.out.println(list);
+	}
+
+	public static <E> ArrayList<E> setToList(Set<E> s) {
+		//create vars
+		ArrayList<E> list = new ArrayList<E>(s);
+		return list;
+	}
+}
