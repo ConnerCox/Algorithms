c844134432ee81d2656f36dd759dd8cb03fb5df9
ConnerCox <49455602+ConnerCox@users.noreply.github.com>
2019-06-11 15:48:40 -0600

    Add files via upload

---
+++ Ch. 28/Exercise27_11.java
@@ -0,0 +1,29 @@
+/* 
+Author: Conner Cox
+Date: June 10th, 2019
+
+Description: Example of turning a set into an Arraylist
+ */
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+
+public class Exercise27_11 {
+	public static void main(String[] args) {
+		Set<String> set = new HashSet<String>(); 
+		set.add("Smith");
+		set.add("Anderson");
+		set.add("Lewis");
+		set.add("Cook"); 
+		set.add("Smith");
+
+		ArrayList<String> list = setToList(set);
+		System.out.println(list);
+	}
+
+	public static <E> ArrayList<E> setToList(Set<E> s) {
+		//create vars
+		ArrayList<E> list = new ArrayList<E>(s);
+		return list;
+	}
+}
